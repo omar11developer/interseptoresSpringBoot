6d322b7bf10c3f585e2b8d9a3e95ad61b5d6ca80
omar11developer <omarmenjivar083@gmail.com>
2023-09-23 15:03:31 -0600

    first commit

---
+++ src/test/java/springboot/horario/HorarioApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.horario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
